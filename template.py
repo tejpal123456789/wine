487b7dd295497935f1bfdf3472f05d86af495aa6
tejpal kumawat <thetajas@gmail.com>
2022-09-01 00:57:32 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,26 @@
+import os
+
+
+# basic structure of the project
+dirs =[
+    os.path.join('data','raw'),
+    os.path.join('data','processed'),
+    'notebooks',
+    'src'
+]
+
+for dir in dirs:
+    os.makedirs(dir,exist_ok=True)
+    with open(os.path.join(dir,'.gitkeep'),'w') as f:
+        pass
+
+files=[
+    'params.yaml',
+    'dvc.yaml',
+    '.gitignore',
+    os.path.join('src','__init__.py')
+]
+
+for file in files:
+    with open(file,'w') as f:
+        pass
