2778f06d62f476e1b60e58dd71aeb401a1fe3baa
tejpal kumawat <thetajas@gmail.com>
2022-09-01 02:18:02 +0530

    stage 1 c

--- src/get_data.py
+++ src/get_data.py
@@ -15,7 +15,8 @@ def get_data(config_path):
     data=read_params(config_path)
     data_path=data['data_source']['s3_source']
     df=pd.read_csv(data_path,sep=',',encoding='utf8')
-    print(df.head())
+    
+    return df
 
     
 if __name__=='__main__':
