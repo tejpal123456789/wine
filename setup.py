ae42dd0091b057b2540a0a3a4c97213a56bea155
tejpal kumawat <thetajas@gmail.com>
2022-09-22 02:12:23 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name='src1',
+      version='0.0.1',
+      description='it is a wine q package ',
+      author='tejpal kumawat',
+      packages=find_packages(),
+      license='MIT')
